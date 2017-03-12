05050697971c8b71e510e5e89791eaf9a1749698
Carlos de Palacio <carlosdepalacio@outlook.com>
2017-03-12 02:29:07 +0100

    Vista del proyecto mejorada

--- server/api/project/project.js
+++ server/api/project/project.js
@@ -34,7 +34,9 @@ router.get('/:id', (req, res) => {
   if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.status(400).json({ message: 'Specified id is not valid' });
   }
-  Project.findById(req.params.id, (err, Projects) => {
+  Project.findById(req.params.id)
+  .populate('_creator')
+  .exec((err, Projects) => {
       if (err) {
         return res.send(err);
       }
