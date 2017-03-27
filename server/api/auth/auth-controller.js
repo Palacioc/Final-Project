74d177b21afb26beb32a85a9dc780edc8e5cbb9d
Carlos de Palacio <carlosdepalacio@outlook.com>
2017-03-27 15:49:47 +0200

    ll

--- server/api/auth/auth-controller.js
+++ server/api/auth/auth-controller.js
@@ -82,6 +82,7 @@ authController.post("/logout", function(req, res) {
 
 //TO CHECK IF USER IS LOGGED IN
 authController.get("/loggedin", function(req, res) {
+  console.log(req.isAuthenticated());
   if(req.isAuthenticated()) {
     return res.status(200).json(req.user);
   }
