b7ee99114cc1c917ce2667161513f158b261d192
Carlos de Palacio <carlosdepalacio@outlook.com>
2017-03-27 14:42:48 +0200

    ff

--- server/configs/passport.js
+++ server/configs/passport.js
@@ -23,12 +23,14 @@ module.exports = function (passport) {
   }));
 
   passport.serializeUser((user, cb) => {
+    console.log("Serialize: ", user);
     cb(null, user.id);
   });
 
   passport.deserializeUser((id, cb) => {
     User.findOne({ "_id": id }, (err, user) => {
       if (err) { return cb(err); }
+      console.log("Deserialize: ", user);
       cb(null, user);
     });
   });
