e453a3ebcfb6d7bf0fa4468d3a546cebebd5d1e4
Carlos de Palacio <carlosdepalacio@outlook.com>
2017-04-10 23:53:01 +0200

    Landing mejorada

--- client/src/app/landing/landing.component.ts
+++ client/src/app/landing/landing.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
+  state = '1';
   constructor() { }
   BASEURL = environment.apiURL;
 
